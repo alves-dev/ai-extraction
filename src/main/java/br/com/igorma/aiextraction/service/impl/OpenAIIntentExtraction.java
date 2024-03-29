a66c44465562c3fa68fbcbb8d33da22b754721ac
igormoreiraalves <igor@biomainvestimentos.com>
2024-03-29 10:55:24 -0300

    CHORE: refactored OpenAIIntentExtraction

--- src/main/java/br/com/igorma/aiextraction/service/impl/OpenAIIntentExtraction.java
+++ src/main/java/br/com/igorma/aiextraction/service/impl/OpenAIIntentExtraction.java
@@ -1,10 +1,13 @@
 package br.com.igorma.aiextraction.service.impl;
 
+import br.com.igorma.aiextraction.database.Extraction;
+import br.com.igorma.aiextraction.database.ExtractionRepository;
 import br.com.igorma.aiextraction.model.Intent;
 import br.com.igorma.aiextraction.model.IntentResponseList;
 import br.com.igorma.aiextraction.service.IntentExtractionService;
 import java.util.List;
 import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.ai.parser.BeanOutputParser;
@@ -15,20 +18,22 @@ import org.springframework.stereotype.Service;
 public class OpenAIIntentExtraction implements IntentExtractionService {
 
     private final OpenAiChatClient aiClient;
+    private final ExtractionRepository repository;
 
     @Autowired
-    public OpenAIIntentExtraction(OpenAiChatClient aiClient) {
+    public OpenAIIntentExtraction(OpenAiChatClient aiClient, ExtractionRepository repository) {
         this.aiClient = aiClient;
+        this.repository = repository;
     }
 
     @Override
-    public IntentResponseList speechToText(String text) {
-        BeanOutputParser<IntentResponseList> outputParser = new BeanOutputParser<>(
-            IntentResponseList.class);
-
+    public IntentResponseList intentExtractionToText(String text) {
         if (text == null || text.length() < 10) {
             return new IntentResponseList(List.of());
         }
+
+        BeanOutputParser<IntentResponseList> outputParser = new BeanOutputParser<>(
+            IntentResponseList.class);
 
         String promptString = """
             Considere o seguinte contexto,
@@ -46,8 +51,11 @@ public class OpenAIIntentExtraction implements IntentExtractionService {
 
         promptTemplate.setOutputParser(outputParser);
 
-        ChatResponse response = aiClient.call(promptTemplate.create());
-        System.out.println(response.getMetadata().getUsage());
-        return outputParser.parse(response.getResult().getOutput().getContent());
+        Prompt prompt = promptTemplate.create();
+        ChatResponse response = aiClient.call(prompt);
+        IntentResponseList result = outputParser.parse(response.getResult().getOutput().getContent());
+
+        repository.save(new Extraction(prompt.getContents(), result, response.getMetadata().getUsage()));
+        return result;
     }
 }
